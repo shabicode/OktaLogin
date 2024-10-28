8368d4bccac0b8b72bb55d1fc672a3520c06a22c
andrea <monicaab.mtz95@gmail.com>
2024-10-28 00:18:27 -0600

    implement login

--- OktaLoginApp/src/app/app.component.ts
+++ OktaLoginApp/src/app/app.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OktaService } from './shared/okta.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'OktaLoginApp';
+  title = 'app works!';
+  user: string | null = null;
+
+  oktaSignIn: any;
+
+  constructor(private okta: OktaService) {
+    
+  }
+
+  showLogin() {
+    this.oktaSignIn.renderEl({el: '#okta-login-container'}, (response: any) => {
+      console.log('response', response);
+      if (response.status === 'SUCCESS') {
+        this.oktaSignIn.tokenManager.add('authToken', response[1]);
+        this.user = response[0].claims.email;
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.oktaSignIn = this.okta.getWidget();
+    this.oktaSignIn.session.get((response: any) => {
+      if (response.status !== 'INACTIVE') {
+        this.user = response.login
+      } else {
+        this.showLogin();
+      }
+    });
+  }
+
+  logout() {
+    this.oktaSignIn.signOut(() => {
+      this.showLogin();
+      this.user = null;
+    });
+  }
 }
--- OktaLoginApp/src/app/shared/okta.service.ts
+++ OktaLoginApp/src/app/shared/okta.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+declare let OktaSignIn: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class OktaService {
+  widget: any;
+  
+  constructor() {
+    this.widget = new OktaSignIn({
+      baseUrl: 'https://dev-87028924.okta.com/oauth2/default',
+      clientId: '0oakn20excsRZPY2w5d7',
+      redirectUri: 'http://localhost:4200',
+      authParams: {
+        responseType: ['id_token', 'token']
+      }
+    })}
 
-  constructor() { }
+   getWidget() {
+    return this.widget;
+   }
 }
