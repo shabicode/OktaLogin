69b8c281215ef76e87b9aaa695be3f77fc39c094
andrea <monicaab.mtz95@gmail.com>
2024-10-29 19:19:15 -0600

    fix env

--- OktaLoginApp/src/app/environments/environment.ts
+++ OktaLoginApp/src/app/environments/environment.ts
@@ -1,8 +1,8 @@
 import { OktaAuthOptions } from "@okta/okta-auth-js";
 
 export const oktaConfig: OktaAuthOptions = {
-    clientId: '0oakn20excsRZPY2w5d7',  
-    issuer: 'https://dev-87028924.okta.com/oauth2/default',
+    clientId: 'xxxx',  
+    issuer: 'https://xxxx-xxxx.okta.com/oauth2/default',
     redirectUri: 'http://localhost:4200/callback',
     scopes: ['openid', 'profile', 'email'],
     responseType: ['code'],  
